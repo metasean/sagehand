6f74470ead76e0f448a499a7da3704306bad88e6
MetaSean <metasean@users.noreply.github.com>
2014-11-21 23:19:05 -0700

    basic festival data display via cards

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,4 +45,116 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FestivalCtrl', function($scope, $stateParams) {
+    $scope.acts = [
+      {
+        "size": "duo",
+        "performer": "Erin & The Project",
+        "act": "",
+        "needs": "Keyboard, Electric Drums, 2 vox, 2 monitors",
+        "plot": "false",
+        "callTime": "11:15am",
+        "setUp": ["11:30am", "11:45am"],
+        "set": ["12:10pm", "13:00pm"]
+      },
+      {
+        "size": "octet",
+        "performer": "Dear Liza",
+        "act": "",
+        "needs": "+1 8 vox, bass, drums, 2 ac guitar(DI), 1 electric guitar(own amp & mic), keyboard, percussionist, guest harmonica, monitors",
+        "plot": "true",
+        "callTime": "",
+        "setUp": ["1:00pm", "1:25pm"],
+        "set": ["1:25pm", "2:15pm"]
+      },
+      {
+        "size": "quintet",
+        "performer": "Alicia Faith",
+        "act": "",
+        "needs": "2 vox, keyboard, 2 guitars (ampad), bass, drums",
+        "plot": "true",
+        "callTime": "",
+        "setUp": ["2:15pm", "2:40pm"],
+        "set": ["2:40pm", "3:30pm"]
+      },
+      {
+        "size": "duo",
+        "performer": "Eliza Gilkyson",
+        "act": "",
+        "needs": "2 vox (will bring 1, needs phantom), guitar w/ pedal board, stomp box(DI), ampad suitor, mandolin (DI), 2 armless chairs, stool 3 monitors",
+        "plot": "true",
+        "callTime": "",
+        "setUp": ["3:30pm", "3:50pm"],
+        "set": ["3:50pm", "4:40pm"]
+      },
+      {
+        "size": "",
+        "performer": "MCs",
+        "act": "Raffle",
+        "plot": "false",
+        "needs": "",
+        "callTime": "",
+        "setUp": [""],
+        "set": ["4:40pm", "5:15pm"]
+      },
+      {
+        "size": "sextet",
+        "performer": "Xperience Freedom",
+        "act": "",
+        "needs": "4 vox, sax, drums, guitar(DI), keyboard",
+        "plot": "false",
+        "callTime": "4:40pm",
+        "setUp": ["5:15pm", "5:25pm"],
+        "set": ["5:25pm", "6:15pm"]
+      },
+      {
+        "size": "duo",
+        "performer": "God Des & She",
+        "act": "",
+        "needs": "2 vox, 2 monitors, guitar, ipod",
+        "plot": "false",
+        "callTime": "5:40pm",
+        "setUp": ["6:15pm", "6:25pm"],
+        "set": ["6:25", "7:15pm"]
+      },
+      {
+        "size": "",
+        "performer": "MCs",
+        "act": "Raffle",
+        "needs": "",
+        "plot": "false",
+        "callTime": "",
+        "setUp": [""],
+        "set": ["7:15", "7:25"]
+      },
+      {
+        "size": "solo",
+        "performer": "Toshi Reagon",
+        "act": "",
+        "needs": "vox, 2 DI, 2 guitar stands, armless chair, stool, 2 monitors",
+        "plot": "true",
+        "callTime": "6:35pm",
+        "setUp": ["7:25pm", "7:30pm"],
+        "set": ["7:30pm", "8:20pm"]
+      },
+      {
+        "size": "",
+        "performer": "MCs & Organizers",
+        "act": "Raffle",
+        "needs": "",
+        "plot": "false",
+        "callTime": "",
+        "setUp": [""],
+        "set": ["8:20pm", "8:50pm"]
+      },
+      {
+        "size": "duo",
+        "performer": "Sarah Bettens",
+        "act": "",
+        "needs": "guitar (DI), keyboard, 2 vox, 4 monitors",
+        "plot": "false",
+        "callTime": "8:00pm",
+        "setUp": ["8:50pm", "9:00pm"],
+        "set": ["9:00pm", "10:30pm"]
+      }
+    ]
 });
